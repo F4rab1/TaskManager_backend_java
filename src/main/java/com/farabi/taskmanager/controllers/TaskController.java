b3d08ad4ef5d1bbcc7f4617c0630e72e16ec6e6c
F4rab1 <issafarabi2022@gmail.com>
2025-11-03 18:20:32 +0100

    Setting HTTP Status Codes

--- src/main/java/com/farabi/taskmanager/controllers/TaskController.java
+++ src/main/java/com/farabi/taskmanager/controllers/TaskController.java
@@ -3,10 +3,13 @@ package com.farabi.taskmanager.controllers;
 import com.farabi.taskmanager.entities.Task;
 import com.farabi.taskmanager.repositories.TaskRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -15,12 +18,18 @@ public class TaskController {
     private final TaskRepository taskRepository;
 
     @GetMapping
-    public Iterable<Task> getAllTasks() {
-        return taskRepository.findAll();
+    public ResponseEntity<List<Task>> getAllTasks() {
+        var tasks = taskRepository.findAll();
+        return ResponseEntity.ok(tasks);
     }
 
     @GetMapping("/{id}")
-    public Task getTaskById(@PathVariable Long id) {
-        return taskRepository.findById(id).orElse(null);
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        var task = taskRepository.findById(id).orElse(null);
+        if (task == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(task);
     }
 }
