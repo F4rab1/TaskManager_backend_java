a433574d0cf4d7ac1c52e5a4c1ffdb074c5771e3
F4rab1 <issafarabi2022@gmail.com>
2025-11-17 10:48:49 +0100

    Users table and entity

---
+++ src/main/java/com/farabi/taskmanager/entities/User.java
@@ -0,0 +1,27 @@
+package com.farabi.taskmanager.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
---
+++ src/main/resources/db/migration/V4__create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE SEQUENCE users_id_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY DEFAULT nextval('users_id_seq'),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
